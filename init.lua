f534d4ac7c49ddcc722bc25c9f3e18b0cd312eaa
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-06-21 14:45:26 +0300

    harpoon VScode check

--- init.lua
+++ init.lua
@@ -50,13 +50,15 @@ if is_wsl() then
     ]])
 end
 
-local harpoon = require("harpoon")
-harpoon:setup()
+if not Vscode then
+    local harpoon = require("harpoon")
+    harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<M-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<M-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
+end
