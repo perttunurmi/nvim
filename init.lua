f0fe2f07cd4ee460039bbc9786b842b06b035737
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-06-23 17:10:58 +0300

    removed duplicate

--- init.lua
+++ init.lua
@@ -7,17 +7,6 @@ else
     Vscode = false
     require 'config.lazy'
     require 'config.colors'
-
-    local harpoon = require("harpoon")
-    harpoon:setup()
-
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<M-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
 end
 
 require 'config.settings'
